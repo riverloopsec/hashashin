0cbf8231eefba6e4eb5c9cd1d124d8b229da480e
riverloopsec <ryan@riverloopsecurity.com>
2019-12-16 16:36:46 -0500

    Updated to allow polytags to be run for dev testing purposes.

--- src/polytags.py
+++ src/polytags.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright 2019 River Loop Security LLC, All Rights Reserved
 # Author Rylan O'Connell
 
@@ -56,7 +58,53 @@ def read_polytags(file: str) -> Dict[str, str]:
                     if leaves is not None:
                         tag_dict.update(leaves)
             return tag_dict
-
     else:
         print('Invalid signature file.')
         sys.exit(-1)
+
+
+def open_binary_in_triage(binary_path: str):
+    print("Starting to load binary {} and set triage settings".format(binary_path))
+    view = binja.BinaryView.open(binary_path)
+    if view is None:
+        print("Failed to load binary.")
+    bns = binja.settings.Settings()
+    print("Starting to load and analyze binary {}...".format(binary_path))
+    bv = None
+    for available in view.available_view_types:
+        if available.name != 'Raw':
+            bv = available.open(binary_path)
+            break
+    bns.set_string('analysis.mode', 'controlFlow', bv)
+    bns.set_integer('analysis.limits.maxFunctionAnalysisTime', 2, bv)
+    bns.set_integer('analysis.maxFunctionAnalysisTime', 2, bv)
+    bns.set_integer('analysis.maxFunctionSize', 500, bv)
+    bns.set_integer('analysis.maxFunctionUpdateCount', 2, bv)
+    bns.set_integer('analysis.limits.cacheSize', 64, view)
+    bns.set_bool("analysis.linearSweep.controlFlowGraph", True, bv)
+    bns.set_bool("analysis.linearSweep.autorun", True, bv)
+    bv.update_analysis_and_wait()
+    print("Loaded binary {} into Binary Ninja: {}.".format(binary_path, bv))
+    return bv
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Apply signatures that were captured from a signature file to a binary.')
+    parser.add_argument('binary',
+                        help='The executable file to attempt to apply signatures onto.')
+    parser.add_argument("--polymerge", required=True,
+                        help="Reads JSON emitted by polymerge tool")
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.binary):
+        print("Must provide valid path to binary.")
+        sys.exit(-1)
+    if not os.path.isfile(args.polymerge):
+        print("Must provide valid path to polymerge JSON file.")
+        sys.exit(-2)
+
+    #bv = open_binary_in_triage(args.binary)
+    functions = read_polytags(args.polymerge)
+    for function, tag in functions.items():
+        print("{: <40}\t{}".format(function.replace('dfs$',''), tag))
